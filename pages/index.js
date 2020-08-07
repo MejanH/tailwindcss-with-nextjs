3c008472fe3c9bbf1a7e7760f5c6f17db8f5fcdc
MejanH <muhammad.mejanul.haque@gmail.com>
2020-08-08 01:38:37 +0600

    tailwind-with-nextjs

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,45 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="prose max-w-none w-3/4 mx-auto mt-10">
       <Head>
-        <title>Create Next App</title>
+        <title>TailwindCSS with Next.js</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <h1>Creating a project with TailwindCSS, NextJS & Typography</h1>
+      <p>
+        <h1>Lorem ipsum dolor sit amet consectetur</h1>
+        Adipiscing, elit feugiat tortor parturient natoque curabitur fusce,
+        penatibus consequat vestibulum odio eros. Nisl suspendisse cras tortor
+        pellentesque molestie aenean tempor lectus nec vestibulum arcu vel,
+        <blockquote>
+          {" "}
+          turpis in leo facilisis fermentum ac platea luctus justo sem.
+        </blockquote>
+        Orci eleifend nisl vel hendrerit justo habitasse dictum inceptos augue
+        rhoncus duis blandit vulputate nostra, felis parturient fringilla
+        conubia platea integer non scelerisque venenatis varius cubilia nullam
+        porta.
+        <pre>
+          <code>
+            {`
+              module.exports = {
+                theme: {
+                  // ...
+                },
+                plugins: [
+                  require('@tailwindcss/typography'),
+                  // ...
+                ],
+              }
+            `}
+          </code>
+        </pre>
+        Ridiculus vel tincidunt sapien ultricies penatibus nulla integer et
+        varius, eros proin mus odio eget cubilia quisque rhoncus ultrices,
+        feugiat semper porta iaculis fames ligula parturient non.
+      </p>
     </div>
-  )
+  );
 }
