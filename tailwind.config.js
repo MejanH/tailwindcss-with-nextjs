a0477a5f8b81e34d9e85c16b7d4591d3b0d72217
MejanH <muhammad.mejanul.haque@gmail.com>
2020-09-15 21:06:25 +0600

    tailwind upcoming chages notice

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
 
   theme: { extend: {} },
   variants: {},
